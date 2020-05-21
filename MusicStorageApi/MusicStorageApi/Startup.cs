39c6d3a8b40bb0baf1bb3bd84405b6ceb0537c1f
Ruslan <Ruslan.Lomaka@Gmail.com>
2020-05-21 13:19:08 +0300

    Swagger was implemented

--- MusicStorageApi/MusicStorageApi/Startup.cs
+++ MusicStorageApi/MusicStorageApi/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using MusicStorageApi.Data.Context;
 using MusicStorageApi.Repositories.Interfaces;
 using MusicStorageApi.Repositories.Repositories;
@@ -30,11 +31,22 @@ namespace MusicStorageApi
             services.AddTransient<IAlbumRepository, AlbumRepository>();
             services.AddTransient<ISongRepository, SongRepository>();
             services.AddControllers().AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MUSIC STORAGE API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MUSIC STORAGE API");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
